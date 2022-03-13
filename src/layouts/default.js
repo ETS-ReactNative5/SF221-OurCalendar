53a41811889f94bd6133474a42865fc2d975c479
Ponlapat Ittiratchapong <webmaster@maxnus.com>
2022-03-13 12:13:30 +0700

    Top navigator

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {Dimensions} from 'react-native';
-import {NativeBaseProvider, HStack, ScrollView, View} from 'native-base';
+import {NativeBaseProvider, HStack, ScrollView, IconButton, Icon, Image} from 'native-base';
+import Octicons from 'react-native-vector-icons/Octicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import Logo from '../assets/logo.png';
 
 const window = Dimensions.get("window");
 
@@ -23,7 +27,15 @@ class DefaultTemplate extends React.Component {
             <NativeBaseProvider>
                 <ScrollView>
                     <HStack bg="#f57f6f" w="100%" h="60">
-                        <></>
+                        <HStack justifyContent="space-between" alignItems="center" w="100%">
+                            <HStack pl="2">
+                                <Image source={Logo} alt="Logo" size="sm" />
+                            </HStack>
+                            <HStack>
+                                <IconButton icon={<Icon as={MaterialCommunityIcons} name="bell-badge" size="md" color="yellow.400" />} />
+                                <IconButton icon={<Icon as={Octicons} name="three-bars" size="md" color="black" />} />
+                            </HStack>
+                        </HStack>
                     </HStack>
                     <HStack bg="#f4be82" w="100%" h="70">
                         <></>

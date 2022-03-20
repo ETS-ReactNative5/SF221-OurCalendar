25520afedc05f9dec04e722d56186ff40c771330
Nickname <patimakorn.ran@dome.tu.ac.th>
2022-03-21 00:06:35 +0700

    create home screen and add event box

---
+++ src/components/molecules/eventBoxHome.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Box, Checkbox, HStack, Icon, Image, Stack, Text} from 'native-base';
+import {StyleSheet} from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+
+
+class EventBoxHead extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Box style={styles.boxEvent} backgroundColor={this.props.color}>
+
+                <HStack justifyContent="flex-end"  paddingTop="4%" paddingRight="10%">
+                    <Text fontSize="13">{this.props.name}</Text>
+                </HStack>
+                <HStack justifyContent="flex-end"  paddingRight="10%">
+                    <Text fontSize="13">{this.props.time}</Text>
+                </HStack>
+                <HStack justifyContent="flex-start">
+                    <Icon as={MaterialCommunityIcons} name={this.props.icon} marginTop="-8" marginLeft="13%"/>
+                </HStack>
+                <HStack justifyContent="flex-start"  paddingLeft="2" marginTop="-45" >
+                    <Checkbox accessibilityLabel="Checkbox"/>
+                </HStack>
+            </Box>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    boxEvent: {
+        width:"80%",
+        height: 70,
+        borderRadius: 12,
+        alignSelf:"center",
+    },
+
+});
+export default EventBoxHead ;
--- src/scenes/home.js
+++ src/scenes/home.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import {Center} from 'native-base';
 import AppTemplate from '../components/templates/app';
+import EventBoxHome from "../components/molecules/eventBoxHome";
+import {Stack, View} from "native-base";
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
     }
-
     render() {
         return (
             <AppTemplate {...this.props}>
-                <Center flex={1}>
-                    Hello I'm Home!
-                </Center>
+                <View width="100%" marginTop="7%" marginBottom="7%">
+                    <Stack space="3">
+                        <EventBoxHome name="วิ่ง" time="7:00-8:00" icon="dumbbell" color="#ffffff"/>
+                        <EventBoxHome name="วิ่ง" time="7:00-8:00" icon="dumbbell"/>
+                        <EventBoxHome name="วิ่ง" time="7:00-8:00" icon="dumbbell"/>
+                        <EventBoxHome name="ersg" time="1:00-8:00" icon="dumbbell"/>
+
+                    </Stack>
+                </View>
             </AppTemplate>
         );
     }

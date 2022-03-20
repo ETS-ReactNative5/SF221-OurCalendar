4f81a7c761062c831f8ce1fa8ee3c3b6ebd9d34d
Ponlapat Ittiratchapong <webmaster@maxnus.com>
2022-03-20 20:41:09 +0700

    Adjust height of AppTemplate

--- src/components/templates/app.js
+++ src/components/templates/app.js
@@ -1,43 +1,31 @@
 import React from 'react';
-import {Dimensions} from 'react-native';
-import {NativeBaseProvider, HStack, ScrollView, Center, Icon, Text, Box} from 'native-base';
+import {NativeBaseProvider, HStack, ScrollView} from 'native-base';
 import Header from '../organisms/headerApp';
 import Bottom from '../organisms/bottomApp';
 
 import WeekDay from "../organisms/weekDay";
 
-const window = Dimensions.get("window");
-
 class AppTemplate extends React.Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            window: {
-                width: window.width,
-                height: window.height,
-            }
-        };
-
     }
 
     render() {
-        const { window: { width, height } } = this.state;
         const { navigation } = this.props;
 
         return (
             <NativeBaseProvider>
-                <ScrollView>
-                    <HStack bg="#f57f6f" w="100%" h="60">
+                <ScrollView contentContainerStyle={{flexGrow: 1}}>
+                    <HStack bg="#f57f6f" w="100%" h="8%">
                         <Header navigation={navigation}/>
                     </HStack>
-                    <HStack bg="#f4be82" w="100%" h="90">
+                    <HStack bg="#f4be82" w="100%" h="12%">
                         <WeekDay/>
                     </HStack>
-                    <HStack bg="#eedec4" w="100%" minH={height - 240}>
+                    <HStack bg="#eedec4" w="100%" minH="72%">
                         {this.props.children}
                     </HStack>
-                    <HStack bg="#9d9fd2" w="100%" h="60">
+                    <HStack bg="#9d9fd2" w="100%" h="8%">
                         <Bottom navigation={navigation}/>
                     </HStack>
                 </ScrollView>

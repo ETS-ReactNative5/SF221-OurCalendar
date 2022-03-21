55f2f86c6e62705d3006a315676b95e94300193b
Ponlapat Ittiratchapong <webmaster@maxnus.com>
2022-03-21 13:16:50 +0700

    Adjust height of AppTemplate (again)

--- src/components/templates/app.js
+++ src/components/templates/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {NativeBaseProvider, HStack, ScrollView} from 'native-base';
+import {NativeBaseProvider, HStack, ScrollView, View} from 'native-base';
 import Header from '../organisms/headerApp';
 import Bottom from '../organisms/bottomApp';
 
@@ -8,6 +8,15 @@ import WeekDay from "../organisms/weekDay";
 class AppTemplate extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            height: 0
+        }
+
+        this.onLayout = this.onLayout.bind(this);
+    }
+
+    onLayout(event) {
+        this.setState({height: event.nativeEvent.layout.height});
     }
 
     render() {
@@ -15,20 +24,22 @@ class AppTemplate extends React.Component {
 
         return (
             <NativeBaseProvider>
-                <ScrollView contentContainerStyle={{flexGrow: 1}}>
-                    <HStack bg="#f57f6f" w="100%" h="8%">
-                        <Header navigation={navigation}/>
-                    </HStack>
-                    <HStack bg="#f4be82" w="100%" h="12%">
-                        <WeekDay/>
-                    </HStack>
-                    <HStack bg="#eedec4" w="100%" minH="72%">
-                        {this.props.children}
-                    </HStack>
-                    <HStack bg="#9d9fd2" w="100%" h="8%">
-                        <Bottom navigation={navigation}/>
-                    </HStack>
-                </ScrollView>
+                <View h="100%" onLayout={this.onLayout}>
+                    <ScrollView>
+                        <HStack bg="#f57f6f" w="100%" h="60">
+                            <Header navigation={navigation}/>
+                        </HStack>
+                        <HStack bg="#f4be82" w="100%" h="90">
+                            <WeekDay/>
+                        </HStack>
+                        <HStack bg="#eedec4" w="100%" minH={this.state.height - 210}>
+                            {this.props.children}
+                        </HStack>
+                        <HStack bg="#9d9fd2" w="100%" h="60">
+                            <Bottom navigation={navigation}/>
+                        </HStack>
+                    </ScrollView>
+                </View>
             </NativeBaseProvider>
         );
     }

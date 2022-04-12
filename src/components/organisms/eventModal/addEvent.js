346aec74dada6d1a93566fe9c25a2e1d018b7d63
Ponlapat Ittiratchapong <webmaster@maxnus.com>
2022-04-13 01:27:09 +0700

    Merge icon selection modal to event modal

--- src/components/organisms/eventModal/addEvent.js
+++ src/components/organisms/eventModal/addEvent.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { withTranslation } from 'react-i18next';
-import {Button, CheckIcon, FormControl, HStack, Input, Modal, Pressable, Select, Text, View} from 'native-base';
+import {Button, CheckIcon, FormControl, HStack, Input, Modal, Select, Text} from 'native-base';
 import moment from 'moment';
 import DatePicker from 'react-native-date-picker';
 import randomId from '../../../utils/randomId';
 import eventStorage from '../../../utils/eventStorage';
 import ColorPicker from 'react-native-wheel-color-picker'
+import IconSelection from "./selectIcon";
 
 class AddEvent extends React.Component {
     constructor(props) {
@@ -18,6 +19,7 @@ class AddEvent extends React.Component {
             openTimeStartEvent: false,
             openDateEndEvent: false,
             openTimeEndEvent: false,
+            iconSelection: false,
 
             colorModal: false,
 
@@ -60,136 +62,139 @@ class AddEvent extends React.Component {
         const { t } = this.props;
 
         return (
-            <Modal isOpen={this.props.isOpen} onClose={this.props.onClose}>
-                <Modal.Content style={styles.addModal} maxWidth="400px">
-                    <Modal.CloseButton />
-                    <Modal.Header><Text>{t('add_event.add')}</Text></Modal.Header>
-                    <Modal.Body>
-                        <FormControl>
-                            <FormControl.Label><Text>{t('event_todo.title')}</Text></FormControl.Label>
-                            <Input onChangeText={(text => this.setState({form: {...this.state.form, title: text}}))} bgColor="#f8f8f8"/>
-                        </FormControl>
-                        <FormControl>
-                            <FormControl.Label><Text>{t('add_event.start')}</Text></FormControl.Label>
-                            <HStack space={3}>
-                                <Button style={styles.selectDate} onPress={() => this.setState({openDateStartEvent: true})}>
-                                    <Text>{moment(this.state.form.start).format("DD MMMM YYYY")}</Text>
-                                </Button>
-                                <DatePicker
-                                    modal
-                                    open={this.state.openDateStartEvent}
-                                    date={this.state.form.start}
-                                    onConfirm={(date) => {
-                                        this.setState({openDateStartEvent: false})
-                                        this.setState({form: {...this.state.form, start: date}})
-                                    }}
-                                    onCancel={() => {
-                                        this.setState({openDateStartEvent: false})
-                                    }}
-                                    minimumDate={new Date()}
-                                    mode={"date"}
+            <>
+                <Modal isOpen={this.props.isOpen} onClose={this.props.onClose}>
+                    <Modal.Content style={styles.addModal} maxWidth="400px">
+                        <Modal.CloseButton />
+                        <Modal.Header><Text>{t('add_event.add')}</Text></Modal.Header>
+                        <Modal.Body>
+                            <FormControl>
+                                <FormControl.Label><Text>{t('event_todo.title')}</Text></FormControl.Label>
+                                <Input onChangeText={(text => this.setState({form: {...this.state.form, title: text}}))} bgColor="#f8f8f8"/>
+                            </FormControl>
+                            <FormControl>
+                                <FormControl.Label><Text>{t('add_event.start')}</Text></FormControl.Label>
+                                <HStack space={3}>
+                                    <Button style={styles.selectDate} onPress={() => this.setState({openDateStartEvent: true})}>
+                                        <Text>{moment(this.state.form.start).format("DD MMMM YYYY")}</Text>
+                                    </Button>
+                                    <DatePicker
+                                        modal
+                                        open={this.state.openDateStartEvent}
+                                        date={this.state.form.start}
+                                        onConfirm={(date) => {
+                                            this.setState({openDateStartEvent: false})
+                                            this.setState({form: {...this.state.form, start: date}})
+                                        }}
+                                        onCancel={() => {
+                                            this.setState({openDateStartEvent: false})
+                                        }}
+                                        minimumDate={new Date()}
+                                        mode={"date"}
+                                    />
+                                    <Button style={styles.selectTime} onPress={() => this.setState({openTimeStartEvent: true})}>
+                                        <Text>{moment(this.state.form.start).format("HH:mm")}</Text>
+                                    </Button>
+                                    <DatePicker
+                                        modal
+                                        open={this.state.openTimeStartEvent}
+                                        date={this.state.form.start}
+                                        onConfirm={(date) => {
+                                            this.setState({openTimeStartEvent: false})
+                                            this.setState({form: {...this.state.form, start: date}})
+                                        }}
+                                        onCancel={() => {
+                                            this.setState({openTimeStartEvent: false})
+                                        }}
+                                        mode={"time"}
+                                    />
+                                </HStack>
+                            </FormControl>
+                            <FormControl>
+                                <FormControl.Label><Text>{t('add_event.end')}</Text></FormControl.Label>
+                                <HStack space={3}>
+                                    <Button style={styles.selectDate} isDisabled={this.state.form.repeat === 'None'} onPress={() => this.setState({openDateEndEvent: true})}>
+                                        <Text>{moment(this.state.form.end).format("DD MMMM YYYY")}</Text>
+                                    </Button>
+                                    <DatePicker
+                                        modal
+                                        open={this.state.openDateEndEvent}
+                                        date={this.state.form.end}
+                                        onConfirm={(date) => {
+                                            this.setState({openDateEndEvent: false})
+                                            this.setState({form: {...this.state.form, end: date}})
+                                        }}
+                                        onCancel={() => {
+                                            this.setState({openDateEndEvent: false})
+                                        }}
+                                        minimumDate={new Date()}
+                                        mode={"date"}
+                                    />
+                                    <Button style={styles.selectTime} onPress={() => this.setState({openTimeEndEvent: true})}>
+                                        <Text>{moment(this.state.form.end).format("HH:mm")}</Text>
+                                    </Button>
+                                    <DatePicker
+                                        modal
+                                        open={this.state.openTimeEndEvent}
+                                        date={this.state.form.end}
+                                        onConfirm={(date) => {
+                                            this.setState({openTimeEndEvent: false})
+                                            this.setState({form: {...this.state.form, end: date}})
+                                        }}
+                                        onCancel={() => {
+                                            this.setState({openTimeEndEvent: false})
+                                        }}
+                                        minimumDate={new Date()}
+                                        mode={"time"}
+                                    />
+                                </HStack>
+                            </FormControl>
+                            <FormControl>
+                                <FormControl.Label><Text>{t('add_event.repeat')}</Text></FormControl.Label>
+                                <Select bgColor="#f8f8f8" selectedValue={this.state.form.repeat} minWidth="200" accessibilityLabel="Choose repeat" placeholder={t('add_event.choose')} _selectedItem={{
+                                    bg: "teal.600",
+                                    endIcon: <CheckIcon size="5" />
+                                }} onValueChange={(item) => this.setState({form: {...this.state.form, repeat: item}})}>
+                                    <Select.Item label={t('add_event.choose_repeat.none')} value="None" />
+                                    <Select.Item label={t('add_event.choose_repeat.daily')} value="Daily" />
+                                    <Select.Item label={t('add_event.choose_repeat.weekly')} value="Weekly" />
+                                    <Select.Item label={t('add_event.choose_repeat.monthly')} value="Monthly" />
+                                    <Select.Item label={t('add_event.choose_repeat.annually')} value="Annually" />
+                                </Select>
+                            </FormControl>
+                            <FormControl>
+                                <FormControl.Label><Text>{t('event_todo.color')}</Text></FormControl.Label>
+                                <ColorPicker
+                                    ref={r => { this.picker = r }}
+                                    color={this.state.currentColor}
+                                    swatchesOnly={this.state.swatchesOnly}
+                                    onColorChange={this.onColorChange}
+                                    onColorChangeComplete={this.onColorChangeComplete}
+                                    thumbSize={20}
+                                    sliderSize={25}
+                                    noSnap={true}
+                                    row={false}
+                                    swatchesLast={this.state.swatchesLast}
+                                    swatches={this.state.swatchesEnabled}
+                                    discrete={this.state.disc}
                                 />
-                                <Button style={styles.selectTime} onPress={() => this.setState({openTimeStartEvent: true})}>
-                                    <Text>{moment(this.state.form.start).format("HH:mm")}</Text>
-                                </Button>
-                                <DatePicker
-                                    modal
-                                    open={this.state.openTimeStartEvent}
-                                    date={this.state.form.start}
-                                    onConfirm={(date) => {
-                                        this.setState({openTimeStartEvent: false})
-                                        this.setState({form: {...this.state.form, start: date}})
-                                    }}
-                                    onCancel={() => {
-                                        this.setState({openTimeStartEvent: false})
-                                    }}
-                                    mode={"time"}
-                                />
-                            </HStack>
-                        </FormControl>
-                        <FormControl>
-                            <FormControl.Label><Text>{t('add_event.end')}</Text></FormControl.Label>
-                            <HStack space={3}>
-                                <Button style={styles.selectDate} isDisabled={this.state.form.repeat === 'None'} onPress={() => this.setState({openDateEndEvent: true})}>
-                                    <Text>{moment(this.state.form.end).format("DD MMMM YYYY")}</Text>
-                                </Button>
-                                <DatePicker
-                                    modal
-                                    open={this.state.openDateEndEvent}
-                                    date={this.state.form.end}
-                                    onConfirm={(date) => {
-                                        this.setState({openDateEndEvent: false})
-                                        this.setState({form: {...this.state.form, end: date}})
-                                    }}
-                                    onCancel={() => {
-                                        this.setState({openDateEndEvent: false})
-                                    }}
-                                    minimumDate={new Date()}
-                                    mode={"date"}
-                                />
-                                <Button style={styles.selectTime} onPress={() => this.setState({openTimeEndEvent: true})}>
-                                    <Text>{moment(this.state.form.end).format("HH:mm")}</Text>
-                                </Button>
-                                <DatePicker
-                                    modal
-                                    open={this.state.openTimeEndEvent}
-                                    date={this.state.form.end}
-                                    onConfirm={(date) => {
-                                        this.setState({openTimeEndEvent: false})
-                                        this.setState({form: {...this.state.form, end: date}})
-                                    }}
-                                    onCancel={() => {
-                                        this.setState({openTimeEndEvent: false})
-                                    }}
-                                    minimumDate={new Date()}
-                                    mode={"time"}
-                                />
-                            </HStack>
-                        </FormControl>
-                        <FormControl>
-                            <FormControl.Label><Text>{t('add_event.repeat')}</Text></FormControl.Label>
-                            <Select bgColor="#f8f8f8" selectedValue={this.state.form.repeat} minWidth="200" accessibilityLabel="Choose repeat" placeholder={t('add_event.choose')} _selectedItem={{
-                                bg: "teal.600",
-                                endIcon: <CheckIcon size="5" />
-                            }} onValueChange={(item) => this.setState({form: {...this.state.form, repeat: item}})}>
-                                <Select.Item label={t('add_event.choose_repeat.none')} value="None" />
-                                <Select.Item label={t('add_event.choose_repeat.daily')} value="Daily" />
-                                <Select.Item label={t('add_event.choose_repeat.weekly')} value="Weekly" />
-                                <Select.Item label={t('add_event.choose_repeat.monthly')} value="Monthly" />
-                                <Select.Item label={t('add_event.choose_repeat.annually')} value="Annually" />
-                            </Select>
-                        </FormControl>
-                        <FormControl>
-                            <FormControl.Label><Text>{t('event_todo.color')}</Text></FormControl.Label>
-                            <ColorPicker
-                                ref={r => { this.picker = r }}
-                                color={this.state.currentColor}
-                                swatchesOnly={this.state.swatchesOnly}
-                                onColorChange={this.onColorChange}
-                                onColorChangeComplete={this.onColorChangeComplete}
-                                thumbSize={20}
-                                sliderSize={25}
-                                noSnap={true}
-                                row={false}
-                                swatchesLast={this.state.swatchesLast}
-                                swatches={this.state.swatchesEnabled}
-                                discrete={this.state.disc}
-                            />
-                        </FormControl>
-                        <FormControl>
-                            <FormControl.Label><Text>{t('event_todo.icon')}</Text></FormControl.Label>
-                            <Button h={30} onPress={this.props.iconButton} />
-                        </FormControl>
-                    </Modal.Body>
-                    <Modal.Footer style={styles.addModal}>
-                        <Button onPress={() => this.onSubmit()}>
-                            <Text color="muted.50">
-                                {t('event_todo.create')}
-                            </Text>
-                        </Button>
-                    </Modal.Footer>
-                </Modal.Content>
-            </Modal>
+                            </FormControl>
+                            <FormControl>
+                                <FormControl.Label><Text>{t('event_todo.icon')}</Text></FormControl.Label>
+                                <Button h={30} onPress={() => this.setState({iconSelection: true})} />
+                            </FormControl>
+                        </Modal.Body>
+                        <Modal.Footer style={styles.addModal}>
+                            <Button onPress={() => this.onSubmit()}>
+                                <Text color="muted.50">
+                                    {t('event_todo.create')}
+                                </Text>
+                            </Button>
+                        </Modal.Footer>
+                    </Modal.Content>
+                </Modal>
+                <IconSelection isOpen={this.state.iconSelection} onClose={() => this.setState({iconSelection: false})}/>
+            </>
         );
     }
 }
--- src/components/templates/eventModalButton.js
+++ src/components/templates/eventModalButton.js
@@ -4,7 +4,6 @@ import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import EventModal from '../organisms/eventModal/eventModal';
 import AddEvent from '../organisms/eventModal/addEvent';
 import AddTodo from '../organisms/eventModal/addTodo';
-import IconSelection from "../organisms/eventModal/selectIcon";
 
 class EventModalButton extends React.Component {
     constructor(props) {
@@ -13,17 +12,12 @@ class EventModalButton extends React.Component {
             eventModal: false,
             addEvent: false,
             addToDo: false,
-            iconSelection: false,
         }
     }
 
     openAddEvent() {
         this.closeModal();
         this.setState({addEvent: true});
-    }
-
-    openIcon() {
-        this.setState({iconSelection: true});
     }
 
     openAddToDo() {
@@ -43,10 +37,6 @@ class EventModalButton extends React.Component {
         this.setState({addToDo: false});
     }
 
-    closeIcon(){
-        this.setState({iconSelection: false});
-    }
-
     render() {
         const { navigation } = this.props;
 
@@ -54,9 +44,8 @@ class EventModalButton extends React.Component {
             <>
                 <IconButton icon={<Icon as={FontAwesome5} name="calendar-plus" size="md" color="black"/>} style={{paddingEnd:0}} onPress={() => this.setState({eventModal: true})} />
                 <EventModal isOpen={this.state.eventModal} onClose={() => this.closeModal()} eventButton={() => this.openAddEvent()} todoButton={() => this.openAddToDo()}/>
-                <AddEvent isOpen={this.state.addEvent} onClose={() => this.closeEvent()} navigation={navigation} iconButton={() => this.openIcon()}/>
+                <AddEvent isOpen={this.state.addEvent} onClose={() => this.closeEvent()} navigation={navigation}/>
                 <AddTodo isOpen={this.state.addToDo} onClose={() => this.closeTodo()} navigation={navigation}/>
-                <IconSelection isOpen={this.state.iconSelection} onClose={() => this.closeIcon()} navigation={navigation}/>
             </>
         );
     }

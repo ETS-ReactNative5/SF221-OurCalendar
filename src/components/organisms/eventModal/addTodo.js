e94e5fd57ce45d435e5d8190d47f366531c0787f
Nickname <patimakorn.ran@dome.tu.ac.th>
2022-04-12 21:53:24 +0700

    todo form function

--- src/components/organisms/eventModal/addTodo.js
+++ src/components/organisms/eventModal/addTodo.js
@@ -3,6 +3,8 @@ import { withTranslation } from 'react-i18next';
 import {Button, FormControl, HStack, Input, Modal, Text} from 'native-base';
 import moment from 'moment';
 import DatePicker from 'react-native-date-picker';
+import randomId from "../../../utils/randomId";
+import todoStorage from "../../../utils/eventStorage";
 
 class AddTodo extends React.Component {
     constructor(props) {
@@ -10,10 +12,39 @@ class AddTodo extends React.Component {
         this.state = {
             openDateEndToDo: false,
             openTimeEndToDo: false,
-            deadlineToDo: new Date(),
+
+            form: {
+                title: '',
+                start: new Date(),
+                end: new Date(),
+                repeat: 'None',
+                color: '',
+                iconFont: '',
+                iconName: ''
+            },
         }
     }
+    async onSubmit() {
+        const id = randomId(4);
+        const eventJson = {
+            id: id,
+            created: new Date(),
+            updated: new Date(),
+            title: this.state.form.title,
+            start: new Date(),
+            end: this.state.form.end,
+            color: this.state.form.color,
+            icon: {
+                font: this.state.form.iconFont,
+                name: this.state.form.iconName,
+            }
+        };
 
+        await todoStorage.insertJson(id, eventJson, 'todos');
+        await todoStorage.insertId(id, 'todoIds');
+
+        this.props.onClose();
+    }
     render() {
         const { t } = this.props;
         return (
@@ -24,41 +55,43 @@ class AddTodo extends React.Component {
                     <Modal.Body>
                         <FormControl>
                             <FormControl.Label><Text>{t('event_todo.title')}</Text></FormControl.Label>
-                            <Input bgColor="#f8f8f8"/>
+                            <Input onChangeText={(text => this.setState({form: {...this.state.form, title: text}}))} bgColor="#f8f8f8"/>
                         </FormControl>
                         <FormControl>
                             <FormControl.Label><Text>{t('add_todo.deadline')}</Text></FormControl.Label>
                             <HStack space={3}>
                                 <Button style={styles.selectDate} onPress={() => this.setState({openDateEndToDo: true})}>
-                                    <Text>{moment(this.state.deadlineToDo).format("DD MMMM YYYY")}</Text>
+                                    <Text>{moment(this.state.form.end).format("DD MMMM YYYY")}</Text>
                                 </Button>
                                 <DatePicker
                                     modal
                                     open={this.state.openDateEndToDo}
-                                    date={this.state.deadlineToDo}
+                                    date={this.state.form.end}
                                     onConfirm={(date) => {
                                         this.setState({openDateEndToDo: false})
-                                        this.setState({deadlineToDo: date})
+                                        this.setState({form: {...this.state.form, end: date}})
                                     }}
                                     onCancel={() => {
                                         this.setState({openDateEndToDo: false})
                                     }}
+                                    minimumDate={new Date()}
                                     mode={"date"}
                                 />
                                 <Button style={styles.selectTime} onPress={() => this.setState({openTimeEndToDo: true})}>
-                                    <Text>{moment(this.state.deadlineToDo).format("HH:mm")}</Text>
+                                    <Text>{moment(this.state.form.end).format("HH:mm")}</Text>
                                 </Button>
                                 <DatePicker
                                     modal
                                     open={this.state.openTimeEndToDo}
-                                    date={this.state.deadlineToDo}
+                                    date={this.state.form.end}
                                     onConfirm={(date) => {
                                         this.setState({openTimeEndToDo: false})
-                                        this.setState({deadlineToDo: date})
+                                        this.setState({form: {...this.state.form, end: date}})
                                     }}
                                     onCancel={() => {
                                         this.setState({openTimeEndToDo: false})
                                     }}
+                                    minimumDate={new Date()}
                                     mode={"time"}
                                 />
                             </HStack>
@@ -73,7 +106,7 @@ class AddTodo extends React.Component {
                         </FormControl>
                     </Modal.Body>
                     <Modal.Footer style={styles.addModal}>
-                        <Button>
+                        <Button onPress={() => this.onSubmit()}>
                             <Text color="muted.50">
                                 {t('event_todo.create')}
                             </Text>
